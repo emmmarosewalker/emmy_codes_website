e0918402e2865156c22296b043311f189af796e6
Emma Walker <emma.w@veyordigital.com>
2022-07-26 18:19:42 +1000

    add animation to hero

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 import cogsIcon from '../../assets/cogs-icon.png';
 import colorPaletteIcon from '../../assets/color-palette-icon.png';
 import githubIcon from '../../assets/github-icon.png';
@@ -34,7 +35,7 @@ const HeroContainer = styled.div`
     `)}
 `;
 
-const HtmlIcon = styled.img`
+const HtmlIcon = styled(motion.img)`
     position: absolute;
     top: ${gridUnitPx(30)};
     right: -${gridUnitPx(15)};
@@ -54,7 +55,7 @@ const HtmlIcon = styled.img`
     `)}
 `;
 
-const MobileIcon = styled.img`
+const MobileIcon = styled(motion.img)`
     position: absolute;
     left: -${gridUnitPx(30)};
     bottom: ${gridUnitPx(30)};
@@ -75,14 +76,14 @@ const MobileIcon = styled.img`
     `)}
 `;
 
-const ColorPaletteIcon = styled.img`
+const ColorPaletteIcon = styled(motion.img)`
     position: absolute;
     left: 40%;
     bottom: -${gridUnitPx(10)};
     width: ${gridUnitPx(120)};
 `;
 
-const CogsIcon = styled.img`
+const CogsIcon = styled(motion.img)`
     position: absolute;
     top: 0;
     left: 0;
@@ -111,18 +112,70 @@ const SocialIconsContainer = styled.div`
     `)}
 `;
 
+const CONTAINER_WIDTH = 1100;
+const CONTAINER_HEIGHT = 600;
+const SPEED_UNIT = 8;
+const SPEED_SLOW = 1 * SPEED_UNIT;
+const SPEED_REGULAR = 2 * SPEED_UNIT;
+const SPEED_FAST = 3 * SPEED_UNIT;
+
 export function Hero() {
+  const x = useMotionValue(550);
+  const y = useMotionValue(300);
+
+  const translateX = useTransform(x, [0, CONTAINER_WIDTH], [-SPEED_REGULAR, SPEED_REGULAR]);
+  const translateY = useTransform(y, [0, CONTAINER_HEIGHT], [-SPEED_REGULAR, SPEED_REGULAR]);
+
+  const translateXSlow = useTransform(x, [0, CONTAINER_WIDTH], [-SPEED_SLOW, SPEED_SLOW]);
+  const translateYSlow = useTransform(y, [0, CONTAINER_HEIGHT], [-SPEED_SLOW, SPEED_SLOW]);
+
+  const translateXFast = useTransform(x, [0, CONTAINER_WIDTH], [-SPEED_FAST, SPEED_FAST]);
+  const translateYFast = useTransform(y, [0, CONTAINER_HEIGHT], [-SPEED_FAST, SPEED_FAST]);
+
+  const handleMouse: MouseEventHandler<HTMLDivElement> = (event) => {
+    x.set(event.pageX);
+    y.set(event.pageY);
+  };
+
   return (
-    <HeroContainer>
-      <HtmlIcon src={htmlIcon} alt="3d html icon" />
-      <MobileIcon src={phoneIcon} alt="3d phone with settings cog" />
+    <HeroContainer onMouseMove={handleMouse}>
+      <HtmlIcon
+        src={htmlIcon}
+        alt="3d html icon"
+        style={{
+          translateX,
+          translateY,
+        }}
+      />
+      <MobileIcon
+        src={phoneIcon}
+        alt="3d phone with settings cog"
+        style={{
+          translateX: translateXFast,
+          translateY: translateYFast,
+        }}
+      />
       <SocialIconsContainer>
         <SocialIcon src={githubIcon} alt="3d github icon" />
         <SocialIcon src={instagramIcon} alt="3d instagram icon" />
       </SocialIconsContainer>
       <DisplayOnlyOnXLarge>
-        <ColorPaletteIcon src={colorPaletteIcon} alt="3d paint color palette" />
-        <CogsIcon src={cogsIcon} alt="3d cogs" />
+        <ColorPaletteIcon
+          src={colorPaletteIcon}
+          alt="3d paint color palette"
+          style={{
+            translateX: translateXSlow,
+            translateY: translateYSlow,
+          }}
+        />
+        <CogsIcon
+          src={cogsIcon}
+          alt="3d cogs"
+          style={{
+            translateX: translateXSlow,
+            translateY: translateYSlow,
+          }}
+        />
       </DisplayOnlyOnXLarge>
       <div>
         <H1 color="white">Title</H1>
