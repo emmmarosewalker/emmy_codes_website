e79d588d1c2ddace407fe3f55a69769143b47750
Emma Walker <emma.w@veyordigital.com>
2022-07-28 19:55:59 +1000

    Start adding mdx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,9 +4,13 @@ import { Layout } from '../../common/Layout';
 import { Tutorials } from '../../common/Tutorials';
 import { Hero } from './Hero';
 
+/* eslint-disable import/no-webpack-loader-syntax */
+import Content from '!@mdx-js/loader!../../content/blog/hello.mdx';
+
 export function Home() {
   return (
     <Layout>
+      <Content />
       <Hero />
       <Contact />
       <Tutorials />
